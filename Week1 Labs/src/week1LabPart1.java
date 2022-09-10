761ea30a681a059cc9190b614d544e41d9ada45c
Shawn Bataju <bshawngreen@gmail.com>
2022-09-09 19:41:10 -0500

    organized code

--- Week1 Labs/src/week1LabPart1.java
+++ Week1 Labs/src/week1LabPart1.java
@@ -2,6 +2,7 @@
 public class week1LabPart1 {
 
 	public static void main(String[] args) {
+		
 		//Declaring and initializing variables
 		int availableSeats = 15;
 		double totalCost = 14.99;
@@ -9,6 +10,7 @@ public class week1LabPart1 {
 		boolean isHotOutside = false;
 		String firstName = "John";
 		String streetAddress = "123 JD Street";
+		
 		//Printing descriptive values of variables
 		System.out.println("There are " + availableSeats + " seats available.");
 		System.out.println("Cost of groceries is " + totalCost);
@@ -16,12 +18,14 @@ public class week1LabPart1 {
 		System.out.println("It is hot outside: " + isHotOutside);
 		System.out.println("First name: " + firstName);
 		System.out.println(firstName + " lives at " + streetAddress);
+		
 		//Updating variable values
 		availableSeats -= 2;
 		totalCost += 2.15;
 		middleInitial = 'B';
 		isHotOutside = !isHotOutside;
 		String fullName = firstName + ' ' + middleInitial + " Doe";
+		
 		//Printing updated/final values
 		System.out.println("\nUpdated available Seats: " + availableSeats);
 		System.out.println("New total cost: " + totalCost);
